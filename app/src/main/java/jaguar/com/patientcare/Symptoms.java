d6b688dd86511e829b00f80f93ada1adda3a6cc3
Trent Rand <Trentmrand@gmail.com>
2015-03-29 14:03:59 -0700

    Initial implementation of Symptoms object

--- app/src/main/java/jaguar/com/patientcare/Symptoms.java
+++ app/src/main/java/jaguar/com/patientcare/Symptoms.java
@@ -6,7 +6,28 @@ package jaguar.com.patientcare;
 
 import com.parse.ParseObject;
 import com.parse.ParseClassName;
+import com.parse.ParseUser;
 
 @ParseClassName("Symptoms")
 public class Symptoms extends ParseObject {
+
+    // Accessor methods for associated User
+    public ParseUser getUser() {
+        return getParseUser("user");
+    }
+
+    public void setUser(ParseUser user) {
+        put ("user", user);
+    }
+
+    // Accessor methods for Pain level
+    public int getPainLevel() {
+        return getInt("painLevel");
+    }
+
+    public void setPainLevel(int painLevel) {
+        put ("painLevel", painLevel);
+    }
+
+    
 }
