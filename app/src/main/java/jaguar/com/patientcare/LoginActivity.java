3cf1bb99e8468bccedbb4fdb130e602e8f208293
Trent Rand <Trentmrand@gmail.com>
2015-03-29 02:08:14 -0700

    Implemented login view
    + Login view UI
    + Login view base functionality

--- app/src/main/java/jaguar/com/patientcare/LoginActivity.java
+++ app/src/main/java/jaguar/com/patientcare/LoginActivity.java
@@ -4,23 +4,53 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import com.parse.*;
 
 public class LoginActivity extends ActionBarActivity {
+
+    // Pointer to UI elements
+    private EditText txtEmail;
+    private EditText txtPassword;
+    private Button btnLogin;
+    private Button btnForgotPassword;
+
+    private String email;
+    private String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        // Initialize pointer to UI elements
+        txtEmail = (EditText) findViewById(R.id.txtemail);
+        txtPassword = (EditText) findViewById(R.id.txtPassword);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnForgotPassword = (Button) findViewById(R.id.btnForgotPassword);
+
+        // Declare UI element event handlers
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnLogin();
+            }
+        });
+
+        btnForgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnForgotPassword();
+            }
+        });
+
         // Enable Local Datastore.
         Parse.enableLocalDatastore(this);
         Parse.initialize(this, "VewvxmlYofAlQkLcRUZjjfJSVf0U9WKeWpWcqfwJ", "K8hbr0l4agNpyJKvRvhmjEBSgkPcvTNrqTVYmxz8");
-
-        // Test Parse
-        ParseObject testObject = new ParseObject("TestObject");
-        testObject.put("foo", "bar");
-        testObject.saveInBackground();
     }
 
 
@@ -45,4 +75,97 @@ public class LoginActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    /**
+     * Called when btnLogin is clicked on activity_login.
+     * Performs error checking before calling attemptStandardLogin method.
+     */
+    public void btnLogin() {
+        // Retrieve text from EditText fields
+        email = (String) txtEmail.getText().toString();
+        password = (String) txtPassword.getText().toString();
+
+        // Check if both credentials are entered. Error check
+        if (email.isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Please enter your Email", Toast.LENGTH_LONG).show();
+            // TODO can we highlight/shake the EditText we are referring to?
+            //End the method early before we attempt standard login
+            return;
+        }
+        if (password.isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Please enter your Password", Toast.LENGTH_LONG).show();
+            // TODO can we highlight/shake the EditText we are referring to?
+            //End the method early before we attempt standard login
+            return;
+        }
+
+        // If email and password are not blank, attempt to login user
+        attemptStandardLogin(email, password);
+    }
+
+    /**
+     * Validates credential parameters by communicating with Parse database backend.
+     * The term "standard" refers to a typical Email / Password credential combination,
+     * as opposed to an optional social media login.
+     * @param email The email used for login attempt
+     * @param password The password used for login attempt
+     */
+    private void attemptStandardLogin(String email, String password) {
+        ParseUser.logInInBackground(email, password, new LogInCallback() {
+            public void done(ParseUser user, ParseException e) {
+                if (user != null) {
+                    // User is logged in successfully
+                    Toast.makeText(getApplicationContext(), "Login successful", Toast.LENGTH_LONG).show();
+                    // TODO cache session token (if required?) and switch to next screen
+                } else {
+                    // Login failed. Details found in <e: ParseException>
+                    // TODO make a displayErrorMessage(Exception e) method - pass, format and Toast the e.getLocalizedMessage()
+                    Toast.makeText(getApplicationContext(), e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    /**
+     * Called when btnForgotPassword is clicked on activity_login.
+     * Performs error checking before calling sendPasswordRecovery method.
+     */
+    public void btnForgotPassword() {
+        // Retrieve text from EditText fields
+        email = (String) txtEmail.getText().toString();
+
+        // Check if an email address was entered. Error check
+        if (email.isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Please enter your Email for password recovery", Toast.LENGTH_LONG).show();
+            // TODO can we highlight/shake the EditText we are referring to?
+            //End the method early before we attempt standard login
+            return;
+        }
+        // If the Email field is not blank, send the password recovery email
+        sendPasswordRecovery(email);
+    }
+
+    /**
+     * Sends a password recovery email to the specified email address.
+     * @param recoveryEmail The email address to send password recovery email
+     */
+    private void sendPasswordRecovery(final String recoveryEmail) {
+        ParseUser.requestPasswordResetInBackground(recoveryEmail,
+                new RequestPasswordResetCallback() {
+                    public void done(ParseException e) {
+                        if (e == null) {
+                            // An email was successfully sent with reset instructions.
+                            Toast.makeText(getApplicationContext(), "Password recovery email has been sent to " + recoveryEmail, Toast.LENGTH_LONG).show();
+                            // Disable btnForgotPassword so you can't spam email
+                            // TODO implement timer so button is re-enabled, just in case they sent to wrong email or something
+                            btnForgotPassword.setEnabled(false);
+                        } else {
+                            // Password recovery failed. Details found in <e: ParseException>
+                            // TODO make a displayErrorMessage(Exception e) method - pass, format and Toast the e.getLocalizedMessage()
+                            Toast.makeText(getApplicationContext(), e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
 }
