39d9b5be18e39b2c10d0636ffd5e7c26b1f7bd0e
Cowgurt <graemedrucker@gmail.com>
2015-04-13 19:01:10 -0700

    registering users works now
    all users are the same type at the moment

--- app/src/main/java/jaguar/com/patientcare/RegisterPatientActivity.java
+++ app/src/main/java/jaguar/com/patientcare/RegisterPatientActivity.java
@@ -1,6 +1,6 @@
 package jaguar.com.patientcare;
 
-//import android.content.Intent;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,6 +11,7 @@ import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.EditText;
 
+import com.parse.ParseUser;
 import com.parse.*;
 
 /**
@@ -22,7 +23,7 @@ public class RegisterPatientActivity extends ActionBarActivity{
     private EditText txtEmail;
     private EditText txtPassword;
     private Button btnRegister;
-    private Spinner spnUserType;
+    //private Spinner spnUserType;
     private ParseUser user;
 
     @Override
@@ -30,22 +31,24 @@ public class RegisterPatientActivity extends ActionBarActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_patient);
         
-        txtEmail = (EditText) findViewById(R.id.email);
-        txtPassword = (EditText) findViewById(R.id.password);
-        btnRegister = (Button) findViewById(R.id.button);
-        spnUserType = (Spinner) findViewById(R.id.spinner);
-        
+        txtEmail = (EditText) findViewById(R.id.regEmail);
+        txtPassword = (EditText) findViewById(R.id.regPassword);
+        btnRegister = (Button) findViewById(R.id.regButton);
+        //spnUserType = (Spinner) findViewById(R.id.regSpinner);
+
         btnRegister.setOnClickListener(new View.OnClickListener(){
 	        @Override
 	        public void onClick(View v){
 		        btnRegister();
 	        }
         });
+
+        Parse.initialize(this, "VewvxmlYofAlQkLcRUZjjfJSVf0U9WKeWpWcqfwJ", "K8hbr0l4agNpyJKvRvhmjEBSgkPcvTNrqTVYmxz8");
     }
     
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
-	    getMenuInflater().inflate(R.menu.menu_login, menu);
+	    getMenuInflater().inflate(R.menu.menu_register, menu);
 	    return true;
     }
     
@@ -63,52 +66,56 @@ public class RegisterPatientActivity extends ActionBarActivity{
 	    
 	    return super.onOptionsItemSelected(item);
     }
-    
+
     public void btnRegister(){
 	    //get text from EditText fields
-	    email = (String) txtEmail.getText().toString().trim();
-	    password = (String) txtPassword.getText().toString().trim();
-	    //insert spinner stuff here
+	    email = txtEmail.getText().toString().trim();
+	    password = txtPassword.getText().toString().trim();
 	    
 	    //makes sure both fields are filled
 	    if(email.isEmpty()){
 		    txtEmail.setError("Please enter an email address");
 		    return; //ends method without registration
 	    }
-	    
 	    if(password.isEmpty()){
 		    txtPassword.setError("Please enter a password");
 		    return; //ends method without registration
 	    }
-	    
+
 	    registerPatient(email, password); //register user if all fields are filled
     }
 
-    public void spnUserType(){
+    //public void spnUserType(){
         //spinner stuff
-    }
+    //}
     
-    public void registerPatient(String email, String password){
-	    ParseUser user = new ParseUser();
-	    user.setUsername(email);
-	    user.setEmail(email);
-	    user.setPassword(password);
-	    
-	    //user.put("userType", "data");
-	    
-	    //Toast.makeText(getApplicationContext(), password, Toast.LENGTH_SHORT).show();
-	    
-	    user.signUpInBackground(new SignUpCallback() {
-		    @Override
-		    public void done(ParseException e){
-			    if(e == null) {
-				    Toast.makeText(getApplicationContext(), "Registration successful", Toast.LENGTH_SHORT).show();
-			    }
-			    else {
-				    //Toast.makeText(getApplicationContext(), "I AM ERROR", Toast.LENGTH_SHORT).show();
-                    //toastExceptionObject(e);
-			    }
-		    }
-	    });
+    public void registerPatient(String em, String pass){
+	    user = new ParseUser();
+	    user.setUsername(em);
+	    user.setEmail(em);
+	    user.setPassword(pass);
+
+        user.signUpInBackground(new SignUpCallback() {
+            public void done(ParseException e) {
+                if (e == null) {
+                    Intent registerIntent = new Intent(getApplicationContext(),RegisterPatientActivity.class);
+                    startActivity(registerIntent);
+                    Toast.makeText(getApplicationContext(), "Registration Successful", Toast.LENGTH_LONG).show();
+                } else {
+                    toastExceptionObject(e);
+                }
+            }
+        });
+    }
+
+    private void toastExceptionObject(Exception e) {
+        // Retrieve error message from e : Exception
+        String errorMessage = e.getLocalizedMessage();
+
+        // Capitalize the first letter
+        errorMessage = errorMessage.substring(0, 1).toUpperCase() + errorMessage.substring(1, errorMessage.length());
+
+        // Display toast with error message
+        Toast.makeText(getApplicationContext(), errorMessage, Toast.LENGTH_LONG).show();
     }
 }
