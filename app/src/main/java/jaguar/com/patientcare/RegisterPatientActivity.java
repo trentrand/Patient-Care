b1fb97887c2c26d11a2049ef64b884d2b2926d4d
Cowgurt <graemedrucker@gmail.com>
2015-04-17 20:47:38 -0700

    first & last names now included in registration

--- app/src/main/java/jaguar/com/patientcare/RegisterPatientActivity.java
+++ app/src/main/java/jaguar/com/patientcare/RegisterPatientActivity.java
@@ -19,11 +19,15 @@ import com.parse.*;
 public class RegisterPatientActivity extends ActionBarActivity{
 	
 	//Stores input from the EditText fields
+    private String firstName;
+    private String lastName;
     private String email;
     private String password;
     private String userType;
 	
 	//Pointer to UI elements
+    private EditText txtFirstName;
+    private EditText txtLastName;
     private EditText txtEmail;
     private EditText txtPassword;
     private Button btnRegister;
@@ -38,6 +42,8 @@ public class RegisterPatientActivity extends ActionBarActivity{
         setContentView(R.layout.activity_register_patient);
         
         //Initialze pointer to UI elements
+        txtFirstName = (EditText) findViewById(R.id.regFirstName);
+        txtLastName = (EditText) findViewById(R.id.regLastName);
         txtEmail = (EditText) findViewById(R.id.regEmail);
         txtPassword = (EditText) findViewById(R.id.regPassword);
         btnRegister = (Button) findViewById(R.id.regButton);
@@ -50,12 +56,6 @@ public class RegisterPatientActivity extends ActionBarActivity{
 		        btnRegister();
 	        }
         });
-
-        /*ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.user_types, R.layout.activity_register_patient);
-
-        adapter.setDropDownViewResource(R.layout.activity_register_patient);
-
-        spnUserType.setAdapter(adapter);*/
 
         //Initializes the correct Parse database
         Parse.initialize(this, "VewvxmlYofAlQkLcRUZjjfJSVf0U9WKeWpWcqfwJ", "K8hbr0l4agNpyJKvRvhmjEBSgkPcvTNrqTVYmxz8");
@@ -86,12 +86,22 @@ public class RegisterPatientActivity extends ActionBarActivity{
     //as arguments when the 'Register' button is pressed
     public void btnRegister(){
 	    //get text from EditText fields
-	    email = txtEmail.getText().toString().trim();
+        firstName = txtFirstName.getText().toString().trim();
+        lastName = txtLastName.getText().toString().trim();
+        email = txtEmail.getText().toString().trim();
 	    password = txtPassword.getText().toString().trim();
         userType = spnUserType.getSelectedItem().toString();
 	    
-	    //makes sure both fields are filled
-	    if(email.isEmpty()){
+	    //makes sure all fields are filled
+        if(firstName.isEmpty()){
+            txtFirstName.setError("Please enter your first name");
+            return; //ends method without registration
+        }
+        if(lastName.isEmpty()){
+            txtLastName.setError("Please enter your last name");
+            return; //ends method without registration
+        }
+        if(email.isEmpty()){
 		    txtEmail.setError("Please enter an email address");
 		    return; //ends method without registration
 	    }
@@ -100,16 +110,18 @@ public class RegisterPatientActivity extends ActionBarActivity{
 		    return; //ends method without registration
 	    }
 
-	    registerPatient(email, password); //register user if all fields are filled
+	    registerPatient(firstName, lastName, email, password); //register user if all fields are filled
     }
 
     //creates a new ParseUser with the entered credentials
-    public void registerPatient(String em, String pass){
+    public void registerPatient(String fn, String ln, String em, String pass){
 	    
 	    //creates the new ParseUser
 	    user = new ParseUser();
 	    
 	    //sets the username, password, email, and userType
+        user.put("firstName", fn);
+        user.put("lastName", ln);
 	    user.setUsername(em);
 	    user.setEmail(em);
 	    user.setPassword(pass);
